a200fe0935a770a7d549bc2f79d1ce2e82388c76
52xd <admin@52xd.tv>
2020-06-29 22:22:40 +0800

    增加DPlay播放器源码
    增加DPlay播放器源码
    修复其他源码BUG

---
+++ bofangqi/DPlay/addons/adminloginbg/config.php
@@ -0,0 +1,37 @@
+<?php
+
+return array (
+  0 => 
+  array (
+    'name' => 'mode',
+    'title' => '模式',
+    'type' => 'radio',
+    'content' => 
+    array (
+      'fixed' => '固定',
+      'random' => '每次随机',
+      'daily' => '每日切换',
+    ),
+    'value' => 'random',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '根据自身爱好选择',
+    'ok' => '',
+    'extend' => '',
+  ),
+  1 => 
+  array (
+    'name' => 'image',
+    'title' => '固定背景图',
+    'type' => 'image',
+    'content' => 
+    array (
+    ),
+    'value' => 'upload/addon/20200624-1/de7f0b5faf5e34c0268a7d5e0d2dda43.jpg',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '请选择文件...',
+    'ok' => '',
+    'extend' => '',
+  ),
+);
---
+++ bofangqi/DPlay/addons/dplayer/config/config.php
@@ -0,0 +1,63 @@
+<?php
+$player=array (
+  'ads' => 
+  array (
+    'status' => '0',
+    'vip' => '0',
+    'group' => '1',
+  ),
+  'pre' => 
+  array (
+    'status' => '1',
+    'url' => '/addons/dplayer/img/loading.gif',
+  ),
+  'logo' => 
+  array (
+    'status' => '0',
+    'url' => '/addons/dplayer/img/logo.png',
+  ),
+  'copyright' => 
+  array (
+    'status' => '1',
+    'content' => '小刀影院',
+    'url' => 'https://www.52xd.tv/',
+  ),
+  'dp' => 
+  array (
+    'auto' => '1',
+    'last' => '1',
+    'next' => '1',
+    'h5' => NULL,
+  ),
+);
+$pre=array (
+  'ads' => 
+  array (
+    'status' => '0',
+    'time' => '2',
+    'button' => '0',
+    'auth' => '0',
+    'group' => '3',
+  ),
+  'pic' => 
+  array (
+    'status' => '0',
+    'img' => '/addons/dplayer/img/loading.gif',
+    'link' => '',
+    'width' => '',
+    'height' => '',
+  ),
+  'vod' => 
+  array (
+    'status' => '0',
+    'url' => '',
+    'link' => '',
+  ),
+);
+$pause=array (
+  'status' => '1',
+  'pic' => '/addons/dplayer/img/U36c68.jpg',
+  'width' => '',
+  'height' => '',
+  'link' => '',
+);
