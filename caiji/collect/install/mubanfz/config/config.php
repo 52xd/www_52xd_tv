2fd672d3111c4d7b7429010924194cbb76bafa9b
52xd <admin@52xd.tv>
2020-07-07 01:28:27 +0800

    增加多项插件功能
    1、增加采集插件（萌芽v10.33最新破解）
    2、增加图床插件（gy“公有云版”、sy“私有云版”）

---
+++ caiji/collect/install/mubanfz/config/config.php
@@ -0,0 +1,58 @@
+<?php
+$Far=array (
+  'mengqing' => 
+  array (
+    'status' => '1',
+    'terminal' => 'pc|android|ios|weixin|qq|bsl',
+    'interval' => 
+    array (
+      'type' => '2',
+      'time' => '1',
+      'number' => '3',
+    ),
+    'title' => '手动弹窗代码部分',
+    'type' => '2',
+    'content' => 
+    array (
+      'image' => '',
+      'text' => '手动弹窗代码部分
+手动弹窗代码部分',
+    ),
+    'leftbutton' => 
+    array (
+      'name' => '手动弹',
+      'url' => '/',
+    ),
+    'rightbutton' => 
+    array (
+      'name' => '窗代码部分',
+      'url' => '/',
+    ),
+    'snow' => 
+    array (
+      'status' => '1',
+      'type' => '1',
+      'image' => '',
+    ),
+  ),
+  'Hmengqing' => 
+  array (
+    'title' => '',
+    'type' => '0',
+    'content' => 
+    array (
+      'image' => '',
+      'text' => '',
+    ),
+    'leftbutton' => 
+    array (
+      'name' => '',
+      'url' => '',
+    ),
+    'rightbutton' => 
+    array (
+      'name' => '',
+      'url' => '',
+    ),
+  ),
+);
